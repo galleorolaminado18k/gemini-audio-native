ead8dda38ea2b6a2871eb53c14e87461dad1a8d6
galleorolaminado18k <galleorolaminado18k@gmail.com>
2025-08-29 17:40:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -78,5 +78,6 @@ def health():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run siempre expone 8080
+    app.run(host="0.0.0.0", port=port)
+
